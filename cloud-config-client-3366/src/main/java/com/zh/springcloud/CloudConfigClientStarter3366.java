3b45b735d5411b42a4057d3e48383d2d2baf5f51
cellophane <zhangheng2683@gmail.com>
2021-11-04 16:59:15 +0800

    Config Bus配置，配合rabbitmq或者kafka，实现消息的自动全局刷新和定点刷新

---
+++ cloud-config-client-3366/src/main/java/com/zh/springcloud/CloudConfigClientStarter3366.java
@@ -0,0 +1,12 @@
+package com.zh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigClientStarter3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigClientStarter3366.class, args);
+    }
+}
