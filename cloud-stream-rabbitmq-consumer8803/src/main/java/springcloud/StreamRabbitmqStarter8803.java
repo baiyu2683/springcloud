55b98fb9ee7ba6977a2789913231cd06b5cd472e
cellophane <zhangheng2683@gmail.com>
2021-11-07 00:36:18 +0800

    Stream 消息驱动

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/springcloud/StreamRabbitmqStarter8803.java
@@ -0,0 +1,11 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamRabbitmqStarter8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamRabbitmqStarter8803.class, args);
+    }
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/zh/springcloud/service/IMessageProvider.java
@@ -0,0 +1,5 @@
+package com.zh.springcloud.service;
+
+public interface IMessageProvider {
+    String send();
+}
