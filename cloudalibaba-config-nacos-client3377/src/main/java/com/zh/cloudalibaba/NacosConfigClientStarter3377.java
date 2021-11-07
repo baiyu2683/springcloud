7b3daceb43043ae342e6eb295b174382058fc9a8
cellophane <zhangheng2683@gmail.com>
2021-11-08 00:05:09 +0800

    Nacos 配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/zh/cloudalibaba/NacosConfigClientStarter3377.java
@@ -0,0 +1,16 @@
+package com.zh.cloudalibaba;
+
+import jdk.nashorn.internal.objects.NativeObject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientStarter3377 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientStarter3377.class, args);
+    }
+
+}
